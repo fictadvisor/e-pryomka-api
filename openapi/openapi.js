eda9fd699b927914d8eb3f878cf6b9b3d876b06e
Oleh Kovalyshyn <kovalyshinO@gmail.com>
2022-02-20 14:42:27 +0200

    fix swagger

--- openapi/openapi.js
+++ openapi/openapi.js
@@ -7,8 +7,4 @@ const swaggerDocument = YAML.load('./openapi.yml');
 const app = express();
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use((req, res) => {
-    res.header("Access-Control-Allow-Origin", "*")
-})
-
 app.listen(3004, () => console.log('Swagger runs on port 3004'))
